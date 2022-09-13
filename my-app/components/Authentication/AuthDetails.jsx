f7235228e5a17190d36e5fb713d86be808a281ca
Arfan12630 <87868992+Arfan12630@users.noreply.github.com>
2022-09-12 23:12:41 -0400

    created authentication
    Created file to switch between registration and login store

---
+++ my-app/components/Authentication/AuthDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginForm from '../LoginPage/LoginForm.jsx' 
+const AuthDetails = () => {
+  // TODO 
+// will use store and redux to switch between registertration form or login form 
+  return (
+    <div>
+      
+     <LoginForm/>
+    </div>
+  )
+}
+
+export default AuthDetails
