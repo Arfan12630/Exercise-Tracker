517e408e25e333d6dde1111bf95bec99f0d52f9f
Arfan12630 <87868992+Arfan12630@users.noreply.github.com>
2022-09-12 23:17:43 -0400

    Added Header

---
+++ my-app/components/Header/Default.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Default.module.css'
+const Default = () => {
+  return (
+    <>
+    <h1 className={classes.title}> TODO </h1>
+    <h1 className={classes.title} > Will Contain info about project </h1>
+    </>
+  )
+}
+
+export default Default
